b0c475d206036b42aa44bb98e10e2b0c91a1c16d
Rubens Flinco <planetacraftvip@hotmail.com.br>
2021-11-18 09:43:39 -0300

    Update Cabecalho.js

--- src/componentes/Cabecalho.js
+++ src/componentes/Cabecalho.js
@@ -29,11 +29,11 @@ export default function Cabecalho(props) {
           </>))}
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="carousel-control-prev-icon d-none" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="carousel-control-next-icon  d-none" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
       </div>
